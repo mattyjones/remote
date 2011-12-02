ec964cf69437778dd30e2bc51ebdde4a2e24ce2c
urlugal <urlugal@gmail.com>
2011-12-02 12:39:13 -0500

    copyright infomation readme infomation added simple pattern matching to allow the openning of multiple servers at once

---
+++ README.txt
@@ -0,0 +1,28 @@
+Copyright 2011-2012 Matt Jones
+ 
+    This is free software: you can redistribute it and/or modify
+	it under the terms of the General Public License either version 2 of the License, or
+    (at your option) any later version.
+	
+For the latest developments on the project check out <https://github.com/urlugal/remote>.
+
+This is a simple commandline substitute for MRemote.  This will take a server hostname, compare it against a file, translate it to an IP and launch rdesktop with a specific set of opitions.  The file can be created using
+the script MRConversion.  You can use your own file as well, the format is two columns: hostname  IP Address.  As of now it will also do simple pattern matching ex. user enters db and servers that are in the form xx-db-xx, will be opened in seperate windows
+
+---List of Files---
+
+MRConversion.sh
+--This file takes the output xml file from mremote and strips out the hostname and IP
+and creates a two-column list in a text file. 
+
+remote.sh
+-- This will take a server hostname, compare it against a file, translate it to an IP
+and launch rdesktop with a specific set of opitions.  It will also do simple pattern matching
+ex. user enters db and servers are in the form xx-db-xx and it will open all db servers 
+in seperate windows
+
+remote-testing.sh
+--for testing, this mat or may not working depending on the day
+
+COPYING.txt
+--copyright infomation
--- remote.sh
+++ remote.sh
@@ -3,29 +3,38 @@
 # This will take a server hostname, compare it against a file, translate it to an IP
 # and launch rdesktop with a specific set of opitions.  The file can be created using
 # the script MRConversion.  You can use your own file as well, the format is 
-# two columns: hostname  IP Address.
+# two columns: hostname  IP Address.  As of now it will also do simple pattern matching
+# ex. user enters db and servers are in the form xx-db-xx and it will open all db servers 
+# in seperate windows
 #
 #matt jones
 #urlugal@gmail.com 
-#updated 11/28/11
+#updated 12/02/11
 #copyright GPL v2
 
 
-declare Input="$1" # the server hostname you wish to connect to
+declare Input="$1" # the server hostname or pattern you wish to connect to
 declare ServerList="/home/matty/bin/scripts/remote/ServerList.txt" #the file created by MRConversion used for the translation
+declare ServerName="" #the server name, used to ID the window
 declare LocalSharedDisk="$HOME" #set the directory to share with the remote host
 declare SharedDiskName="$USER" #the name of the shared disk on the remote system
 declare Resolution="1024x768" #the desired resolution, replace -g $Resolution with -f for full screen
 declare UserName="" #the desired username
 
 #---this will give me the matching servers to the criteria---#
-set $(grep -c -i $Input $ServerList)  #find the server name in the file and get the IP address
-declare ServerCount=$1 #set the IP address from the above statement
-echo $ServerCount
+set $(grep -c -i $Input $ServerList)
+declare ServerCount=$1
 
+#this will go through the Server List file and match the hostname(s) to IP and run rdektop with the above config
+for (( i = 1; i <= $ServerCount; i++ ))
 
-set $(grep $Input $ServerList | awk '{print $2}') #find the server name in the file and get the IP address
-declare RemoteServer=$1 #set the IP address from the above statement
-
-#run rdesktop
-#rdesktop -g $Resolution -r disk:$SharedDiskName=$LocalSharedDisk -u "$UserName" -0 -T $Input -N $RemoteServer & 
+	do
+		set $(grep -m $i $Input $ServerList | awk -v i="$i" 'NR == i {print $1}') #the host name
+		declare ServerName=$1
+		 
+		set $(grep -m $i $Input $ServerList |  awk -v i="$i" 'NR == i {print $2}')
+		declare RemoteServer=$1 #set the IP address from the above statement
+		
+		rdesktop -g $Resolution -r disk:$SharedDiskName=$LocalSharedDisk -u "$UserName" -0 -T $ServerName -N $RemoteServer & 
+		
+done
